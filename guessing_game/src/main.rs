000ef1c268163569eb2c2d4e2499f5b1531ded94
Poramin Lertudom <poramin.lertudom@gmail.com>
2025-06-18 14:54:19 +0700

    Relearn chapt 1-3

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
@@ -17,22 +16,18 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
-                continue;
-            }
+            Err(_) => continue,
         };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
+            Ordering::Less => println!("Too low!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win");
                 break;
             }
         }
